63a23f876d829f51776433a7ad21330ad586690e
faquini-gilberto <faquini.gilberto@gmail.com>
2020-07-29 15:20:34 -0300

    Category

---
+++ src/main/java/com/faquini/course/resources/CategoryResource.java
@@ -0,0 +1,37 @@
+package com.faquini.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.faquini.course.entities.Category;
+import com.faquini.course.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		//Category u = new Category((long) 1, "Dulce", "dulce@gmail.com", "999999", "ggggg");
+		//return ResponseEntity.ok().body(u);
+		
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+}
