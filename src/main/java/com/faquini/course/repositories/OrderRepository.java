06968dccc3d62a20ce1fd92c47e7c8d77d7b94f9
faquini-gilberto <faquini.gilberto@gmail.com>
2020-07-29 13:52:45 -0300

    Order, Instant, ISO 8601 p2

---
+++ src/main/java/com/faquini/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.faquini.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.faquini.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
