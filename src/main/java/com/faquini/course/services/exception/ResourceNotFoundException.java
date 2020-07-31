c49f4ea41b2d84bae92b4fb024aa11a18d052e76
faquini-gilberto <faquini.gilberto@gmail.com>
2020-07-31 14:51:59 -0300

    Exception handling - findById

---
+++ src/main/java/com/faquini/course/services/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.faquini.course.services.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resouce not found Id " + id);
+	}
+}
